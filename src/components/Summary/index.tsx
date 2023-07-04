2c5f15779f28f504828d77dc858efd483e81a28b
marcosmoraesnf <86791856+marcosmoraesnf@users.noreply.github.com>
2023-07-04 20:47:50 -0300

    desenvolvendo a interface principal

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { Container } from "./style";
+import incomeImg from "../../assets/income.svg";
+import outcomeImg from "../../assets/outcome.svg";
+import totalImg from "../../assets/total.svg";
+
+export const Summary = () => {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+        <strong>R$ 12000,00</strong>
+      </div>
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Entradas" />
+        </header>
+        <strong>- R$ 2000,00</strong>
+      </div>
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Entradas" />
+        </header>
+        <strong>R$ 10000,00</strong>
+      </div>
+    </Container>
+  );
+};
